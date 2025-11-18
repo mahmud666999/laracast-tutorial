28efdb4391bfa6cb94073df9004dbb5dd95c89da
mahmud666999 <mahmud.hassan@g.bracu.ac.bd>
2025-11-18 12:28:30 +0600

    Something went wrong while configuring the Models relations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'remember_token' => Str::random(10)
         ];
     }
 
@@ -41,4 +41,5 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
 }
