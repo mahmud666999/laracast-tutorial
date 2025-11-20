ad9c05229e907826711f2df547d5060d2a77301a
mahmud666999 <mahmud.hassan@g.bracu.ac.bd>
2025-11-20 10:55:16 +0600

    Fixed the foreign pivot key and related pivot key issue

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -17,7 +17,7 @@ class Job extends Model
         return $this->belongsTo(Employer::class);
     }
     public function tags(){
-        return $this->belongsToMany(Tag::class,foreignPivotKey: "job_listing_id");
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
     }
 }
 
